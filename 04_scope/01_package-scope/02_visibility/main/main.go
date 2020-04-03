a9c322d907e7ae5b671cca54191875bdd5f60642
rachmat-hidayat <rachmat.hidayat@rea-group.com>
2020-04-04 02:03:29 +0800

    fix import url

--- 04_scope/01_package-scope/02_visibility/main/main.go
+++ 04_scope/01_package-scope/02_visibility/main/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/rhidayat1980/golang-training/04_scope/02_visibility/vis"
+	"github.com/rhidayat1980/golang-training/04_scope/01_package-scope/02_visibility/vis"
 )
 
 func main() {
-	fmt.Println(vis.Myname)
+	fmt.Println(vis.MyName)
+	fmt.Println(vis.YourName)
+
+	// print using an exported function inside vis package
 	vis.PrintVar()
 }
+
+/*
+to remove ugly import use go mod instead.
+*/
